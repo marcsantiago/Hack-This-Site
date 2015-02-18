2e1e6f21b58b7f38cdd4508e150af71629cd2acc
marcsantiago <marcanthonysanti@gmail.com>
2015-02-17 23:15:45 -0500

    Update HackThisSite.py

--- HackThisSite.py
+++ HackThisSite.py
@@ -1,15 +1,18 @@
-import urllib, urllib2, cookielib
+import urllib
+import urllib2
+import cookielib
+
 
 class HackThisSite:
     
     # create constructor that takes user name and password and pass tht data to the login methods
     my_username = ""
-    my_password= ""
+    my_password = ""
+
     def __init__(self, username, password):
         self.my_username = username
         self.my_password = password
-    
-    
+
     values = {'Host': 'www.hackthissite.org',
               'User-Agent':'Mozilla/4.0 (compatible; MSIE 8.0)',
               'Referer':'http://www.hackthissite.org/user/login',
@@ -19,14 +22,14 @@ class HackThisSite:
     
     # This allows us to login as well as read the page we want
     def login_and_read_page_we_want(self, link__to_page_we_want_to_open):
-        '''Just pass it the url to the page where the hts problem is'''
+        """Just pass it the url to the page where the hts problem is"""
         self.host = 'www.hackthissite.org'
         self.user_agent = 'Mozilla/4.0 (compatible; MSIE 6.0)'
         self.referer = 'http://www.hackthissite.org/user/login'
         self.content_type = 'application/x-www-form-urlencoded'
         self.accept_encoding = 'gzip,deflate'
-        self.body = {'username': self.my_username, 'password': self.my_password}        
-        
+        self.body = {'username': self.my_username, 'password': self.my_password}
+
         #build cookie handler
         self.cj = cookielib.CookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
@@ -40,7 +43,7 @@ class HackThisSite:
         self.req = urllib2.Request(self.referer, self.data, self.values)
         self.page = urllib2.urlopen(self.req)
         
-        #return the html page we are are going be mulipulating 
+        #return the html page we are are going to work with
         self.url = link__to_page_we_want_to_open
         self.page = urllib2.urlopen(self.url)
         
@@ -55,12 +58,12 @@ class HackThisSite:
                 pieces.extend(substr.split(char))
                 stack = pieces
         return stack
-    
 
-    
     # Submit hts form data
     def send_answer(self, data_to_send, page_to_send_data_to):
-        self.form_data = {'solution' : data_to_send} # for mission 12 you will need to change the code to form_data = {'solution' : data_to_send, "submitbutton" : "Submit"}
+        #for mission 12 you will need to change the code to
+        #form_data = {'solution' : data_to_send, "submitbutton" : "Submit"}
+        self.form_data = {'solution' : data_to_send}
         self.encoded_data = urllib.urlencode(self.form_data)
         self.referer = page_to_send_data_to
         self.req = urllib2.Request(self.referer, self.encoded_data, self.values)
